ac748524ae112cd92ebea37ac6104939378e9750
deeploid <erniecyc@gmail.com>
2021-05-30 21:35:54 +0200

    fix errors

--- train_scripts/run_bart.sh
+++ train_scripts/run_bart.sh
@@ -107,28 +107,29 @@ ARCH=bart_base;
 # ARCH=transformer_wmt_en_de;
 
 train_translate() {
-    python train.py \
-    $1 \
+    fairseq-train $1 \
     --save-dir checkpoints \
     --ddp-backend=no_c10d \
     --task translation_lev \
     --criterion nat_loss \
-    --arch levenshtein_transformer \
-    --noise random_delete \
+    --arch nonautoregressive_transformer \
+    --noise full_mask \
     --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9,0.98)' \
     --lr 0.0005 --lr-scheduler inverse_sqrt \
-    --warmup-updates 10000 \
+    --stop-min-lr '1e-09' --warmup-updates 10000 \
     --warmup-init-lr '1e-07' --label-smoothing 0.1 \
     --dropout 0.3 --weight-decay 0.01 \
     --decoder-learned-pos \
     --encoder-learned-pos \
+    --pred-length-offset \
+    --length-loss-factor 0.1 \
     --apply-bert-init \
     --log-format 'simple' --log-interval 100 \
     --fixed-validation-seed 7 \
     --max-tokens 8000 \
     --save-interval-updates 10000 \
-    --max-update 300000
+    --max-update 300000;
 }
 
 
