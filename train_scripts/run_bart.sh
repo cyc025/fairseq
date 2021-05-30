a9dbede863a7922b0816909ca8885f8ec97c4e7c
deeploid <erniecyc@gmail.com>
2021-05-30 11:42:42 +0200

    fix errors

--- train_scripts/run_bart.sh
+++ train_scripts/run_bart.sh
@@ -107,7 +107,7 @@ ARCH=bart_base;
 # ARCH=transformer_wmt_en_de;
 
 train_translate() {
-    python train.py $1  \
+    fairseq-train $1  \
     --max-tokens 4400 \
     --task $TASK \
     --add-prev-output-tokens \
@@ -151,7 +151,7 @@ generate() {
 ########################
 
 echo "Preprocessing documents."
-preprocess;
+# preprocess;
 
 ####################
 ## translation  ##
