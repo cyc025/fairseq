d8b2d8951b9ff4915645cf8055e6df4088c05cdb
deeploid <erniecyc@gmail.com>
2021-06-04 13:22:10 +0200

    fix errors

--- fairseq/tasks/translation_lev.py
+++ fairseq/tasks/translation_lev.py
@@ -134,7 +134,7 @@ class TranslationLevenshteinTask(TranslationTask):
                 target_cutoff.scatter(1, target_rank, target_cutoff), unk
             )
 
-            from fairseq import pdb; pdb.set_trace()
+            # from fairseq import pdb; pdb.set_trace()
             return prev_target_tokens
 
         def _full_mask(target_tokens):
