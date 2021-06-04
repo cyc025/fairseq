9a7cf548f9b1606f41e601299eb4fc994f5c7ad6
deeploid <erniecyc@gmail.com>
2021-06-04 16:14:20 +0200

    fix errors

--- fairseq/tasks/translation_lev.py
+++ fairseq/tasks/translation_lev.py
@@ -121,7 +121,7 @@ class TranslationLevenshteinTask(TranslationTask):
             # p defines masking probability
             p = 0.9
             target_length = target_masks.sum(1).float()
-            target_length = target_length * target_length.clone().uniform_(p,p)
+            target_length = target_length * target_length.clone().uniform_()
             target_length = target_length + 1  # make sure to mask at least one token.
 
             # masking by checking if each index is smaller than target mask length,
