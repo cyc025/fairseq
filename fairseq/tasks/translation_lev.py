22da9f79a25672071ffce3f43a3f15373199d1a1
deeploid <erniecyc@gmail.com>
2021-06-06 22:30:01 +0200

    fix errors

--- fairseq/tasks/translation_lev.py
+++ fairseq/tasks/translation_lev.py
@@ -129,11 +129,11 @@ class TranslationLevenshteinTask(TranslationTask):
             target_length = target_masks.sum(1).float()
 
             # get ratios
-            end_ratio = target_length.clone().uniform_(0.9,1.0)
-            start_ratio = target_length.clone().uniform_(0.7,0.7)
-
-            start_point = target_length * start_ratio
-            start_point = start_point + 1  # make sure to mask at least one token.
+            end_ratio = target_length.clone().uniform_(0.9,0.9)
+            # start_ratio = target_length.clone().uniform_(0.7,0.7)
+            #
+            # start_point = target_length * start_ratio
+            # start_point = start_point + 1  # make sure to mask at least one token.
             target_length = target_length * end_ratio
             target_length = target_length + 1  # make sure to mask at least one token.
 
@@ -145,10 +145,10 @@ class TranslationLevenshteinTask(TranslationTask):
             # 'new_arange(target_rank)' contains the iteration of indices (zero-index)
             _, target_rank = target_score.sort(1)
             target_cutoff = new_arange(target_rank) < target_length[:, None].long()
-            start_cutoff = new_arange(target_rank) > start_point[:, None].long()
-            final_cutoff = start_cutoff & target_cutoff
+            # start_cutoff = new_arange(target_rank) > start_point[:, None].long()
+            # final_cutoff = start_cutoff & target_cutoff
             prev_target_tokens = target_tokens.masked_fill(
-                final_cutoff.scatter(1, target_rank, final_cutoff), unk
+                target_cutoff.scatter(1, target_rank, target_cutoff), unk
             )
             # from fairseq import pdb; pdb.set_trace()
             return prev_target_tokens
