fc7dff278027b797d6d7708016b7b44e51cd33d3
deeploid <erniecyc@gmail.com>
2021-06-07 07:46:23 +0200

    fix errors

--- train_scripts/run_bart.sh
+++ train_scripts/run_bart.sh
@@ -133,7 +133,7 @@ train_translate() {
     --task $TASK \
     --criterion nat_loss \
     --arch $ARCH \
-    --noise full_mask \
+    --noise no_noise \
     --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9,0.98)' \
     --lr $LR --lr-scheduler inverse_sqrt \
