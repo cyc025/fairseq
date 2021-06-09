86262d1711d85bc259248cf8edc7c1b842b278bc
deeploid <erniecyc@gmail.com>
2021-06-09 20:35:41 +0200

    fix errors

--- fairseq/tasks/translation_lev.py
+++ fairseq/tasks/translation_lev.py
@@ -207,7 +207,7 @@ class TranslationLevenshteinTask(TranslationTask):
                 return final_cutoff.scatter(1, target_rank, final_cutoff)
 
             ## choose mask distribution
-            mask_patterns = multi_segment()
+            mask_patterns = uniform_original()
 
             # masking
             prev_target_tokens = target_tokens.masked_fill(
