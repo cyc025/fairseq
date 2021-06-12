dbef579ceea9af542d82bb15f6eabedf243ce06c
deeploid <erniecyc@gmail.com>
2021-06-12 10:22:17 +0200

    fix errors

--- fairseq/tasks/translation_lev.py
+++ fairseq/tasks/translation_lev.py
@@ -140,7 +140,14 @@ class TranslationLevenshteinTask(TranslationTask):
                 start_ratio = mask_distribution
                 return map_single_segment(start_ratio,end_ratio)
 
-            def predict_start_uniform_end_relaxed_v2(): # 24.61
+            def predict_start_uniform_end_relaxed_v3(): #
+                """ DyMask-v1 (predict start, uniform end positions) """
+                nonlocal target_length
+                end_ratio = target_length.clone().uniform_(0.97,1.0)
+                start_ratio = mask_distribution
+                return map_single_segment(start_ratio,end_ratio)
+
+            def predict_start_uniform_end_relaxed_v2(): # 24.83
                 """ DyMask-v1 (predict start, uniform end positions) """
                 nonlocal target_length
                 end_ratio = target_length.clone().uniform_(0.95,1.0)
@@ -223,7 +230,7 @@ class TranslationLevenshteinTask(TranslationTask):
             logger.info(mask_distribution)
 
             ## choose mask distribution
-            mask_patterns = predict_start_uniform_end_relaxed_v2()
+            mask_patterns = predict_start_uniform_end_relaxed_v3()
 
             # masking
             prev_target_tokens = target_tokens.masked_fill(
