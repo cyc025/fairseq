cd41b61909c80c0777d1ebeaecb3e322ad3a54f1
deeploid <erniecyc@gmail.com>
2021-06-18 11:01:55 +0200

    fix errors

---
+++ bart_summarizer.py
@@ -0,0 +1,24 @@
+
+
+import torch
+import torchvision.models as models
+from torch.profiler import profile, record_function, ProfilerActivity
+
+from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
+
+model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+
+ARTICLE_TO_SUMMARIZE = "My friends are cool but they eat too many carbs."
+inputs = tokenizer([ARTICLE_TO_SUMMARIZE], max_length=1024, return_tensors='pt')
+
+
+def inference():
+    # Generate Summary
+    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=5, early_stopping=True)
+    # print([tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids])
+
+
+with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    with record_function("model_inference"):
+        inference()
