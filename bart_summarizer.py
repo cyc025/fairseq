d05afd8427169d961ad37adf53ea4a69720de883
deeploid <erniecyc@gmail.com>
2021-06-18 11:35:12 +0200

    fix errors

--- bart_summarizer.py
+++ bart_summarizer.py
@@ -13,14 +13,22 @@ ARTICLE_TO_SUMMARIZE = "My friends are cool but they eat too many carbs."
 inputs = tokenizer([ARTICLE_TO_SUMMARIZE], max_length=1024, return_tensors='pt')
 
 
-def inference():
+
+
+def inference(max_length):
     # Generate Summary
-    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=5, early_stopping=True)
+    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=max_length, early_stopping=True)
     # print([tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids])
 
 
-# with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
-with profile(use_cuda=False) as prof:
-    with record_function("model_inference"):
-        inference()
-print(prof)
+# open file for profiling
+profile_log = open('profile.log','a')
+
+for ml in range(1,2):
+    # with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    with profile(use_cuda=False) as prof:
+        with record_function("model_inference"):
+            inference(ml)
+    profile_log.write(prof+'\n')
+
+profile_log.close()
