d408cffa76f8929e0c282720d538471e9479e2fe
deeploid <erniecyc@gmail.com>
2021-06-19 15:17:08 +0200

    fix errors

--- bash_scripts/profiler.sh
+++ bash_scripts/profiler.sh
@@ -2,7 +2,7 @@
 
 upperlim=17
 for ((i=16; i<=upperlim; i++)); do
-  echo i > .max.len
+  echo $i > .max.len
   taskset --cpu-list 1 python examples/bart/summarize.py   --model-dir .   --model-file checkpoints/bart.large.cnn/model.pt   --src ~/fairseq_cnn_data/cnn_cln/toy_test.source   --out ~/fairseq_cnn_data/cnn_cln/test.hypo;
   rm .max.len
 done
