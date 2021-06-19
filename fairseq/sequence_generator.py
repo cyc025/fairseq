4600353619c2cc9634abdd098ab0307b61d0408e
deeploid <erniecyc@gmail.com>
2021-06-19 08:41:29 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -313,6 +313,7 @@ class SequenceGenerator(nn.Module):
 
         # from fairseq import pdb; pdb.set_trace()
         max_len = 4
+        self.search.stop_on_max_len = True
 
         for step in range(max_len + 1):  # one extra step for EOS marker
 
