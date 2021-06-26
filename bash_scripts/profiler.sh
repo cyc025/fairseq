32f6e294152dccae6663b27ef8aaf841851a9a97
deeploid <erniecyc@gmail.com>
2021-06-26 16:38:36 +0200

    fix errors

--- bash_scripts/profiler.sh
+++ bash_scripts/profiler.sh
@@ -12,9 +12,9 @@
 # to be deleted
 # alias with-proxy='env http_proxy=fwdproxy:8080 https_proxy=fwdproxy:8080 no_proxy=.fbcdn.net,.facebook.com,.thefacebook.com,.tfbnw.net,.fb.com,.fburl.com,.facebook.net,.sb.fbsbx.com,localhost RSYNC_PROXY=fwdproxy:8080 HTTP_PROXY=http://fwdproxy:8080 HTTPS_PROXY=http://fwdproxy:8080'
 
-upperlim=2
+upperlim=50
 for ((i=2; i<=upperlim; i++)); do
-    for ((j=1; j<=2; j++)); do
+    for ((j=1; j<=10; j++)); do
         echo $i > .curr_index
         echo $i > .max.len
         # taskset --cpu-list 1 python examples/bart/summarize.py   --model-dir data-bin/cnn_dm --model-file ~/checkpoints/bart.large.cnn/model.pt   --src data-bin/cnn_dm/toy_test.source   --out data-bin/cnn_dm/test.hypo;
