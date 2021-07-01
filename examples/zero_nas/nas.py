1a43bdc8b7dedb723c7b9adb5840e616d64bdc4a
deeploid <erniecyc@gmail.com>
2021-07-01 12:16:43 +0200

    fix errors

--- examples/zero_nas/nas.py
+++ examples/zero_nas/nas.py
@@ -35,7 +35,7 @@ os.system("rm checkpoints/transformer_wikitext-2/*")
 
 params = {
     'decoder_embed_dim': 1000,
-    'decoder_layers': 18,
+    'decoder_layers': 10,
     'decoder_attention_heads': 10,
 }
 parameters = f"--decoder-embed-dim {params['decoder_embed_dim']} \
