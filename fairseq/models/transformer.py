6e6988a7def3356895445b74c19c102f51016263
deeploid <erniecyc@gmail.com>
2021-07-01 12:41:10 +0200

    fix errors

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -973,7 +973,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
             sigmas = torch.load('sigmas.pt')
             new_sigmas = []
             buffer_val = 100
-            print(init_x.grad.mean().cpu().numpy())
+            # print(init_x.grad.mean().cpu().numpy())
             for sigma in sigmas:
                 C_dim = sigma.size()[0]
                 new_sigmas.append(torch.sqrt( torch.sum(torch.pow(sigma, 2)) / C_dim * buffer_val ))
