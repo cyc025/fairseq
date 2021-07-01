a8c753ef4a524efb56180808eee44a136360a49e
deeploid <erniecyc@gmail.com>
2021-07-01 12:01:33 +0200

    fix errors

--- examples/zero_nas/nas.py
+++ examples/zero_nas/nas.py
@@ -50,6 +50,7 @@ search_command = f"python train.py --task language_modeling \
           --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
           --tokens-per-sample 512 --sample-break-mode none \
           --max-tokens 2048 --update-freq 16 \
+          --disable-validation \
           --fp16 \
           --max-update 1"
 os.system(search_command)
