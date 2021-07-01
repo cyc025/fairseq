19303f54529cb2774f2e3053aaa7b09d69e5d297
deeploid <erniecyc@gmail.com>
2021-07-01 11:28:45 +0200

    fix errors

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -991,7 +991,7 @@ class TransformerDecoder(FairseqIncrementalDecoder):
         #         attn_grad = layer_attn_grad.float().to(x_grad)
 
         from fairseq import pdb; pdb.set_trace()
-        x.mean().detach().backward(retain_graph=True)
+        x.mean().backward(retain_graph=True)
 
         sigmas = torch.load('sigmas.pt')
         new_sigmas = []
