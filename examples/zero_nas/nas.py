6c4c5cf4049b014654cf67243f61ea1ee55e694e
deeploid <erniecyc@gmail.com>
2021-07-01 13:31:45 +0200

    fix errors

--- examples/zero_nas/nas.py
+++ examples/zero_nas/nas.py
@@ -69,9 +69,9 @@ decoder_embed_dims = range(50,2000,50)
 decoder_layerss = range(1,10)
 decoder_attention_headss = range(1,10)
 
-decoder_embed_dims = range(50,100,50)
-decoder_layerss = range(1,2)
-decoder_attention_headss = range(1,2)
+decoder_embed_dims = range(50,200,50)
+decoder_layerss = range(1,4)
+decoder_attention_headss = range(1,3)
 
 zen_scores_tups = []
 with open('search.log','w') as search_log:
