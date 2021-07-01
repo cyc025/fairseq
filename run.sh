685485f755651db5a5be7df274022422ee47d1c8
deeploid <erniecyc@gmail.com>
2021-07-01 17:31:17 +0200

    fix errors

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+python examples/bart/summarize.py   --model-dir .   --model-file checkpoints/bart.base/model.pt   --src ~/fairseq_cnn_data/cnn_cln/toy_test.source   --out ~/fairseq_cnn_data/cnn_cln/test.hypo;
