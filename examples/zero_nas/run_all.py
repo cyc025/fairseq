bbf0af66e7c5672bd6184b0ca6c023825676fdc3
deeploid <erniecyc@gmail.com>
2021-07-04 19:38:08 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -30,7 +30,6 @@ def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
               --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
               --tokens-per-sample 512 --sample-break-mode none \
               --max-tokens 2048 --update-freq 16 \
-              --disable-validation \
               --fp16 \
               --batch-size 1024 \
               {parameters} \
