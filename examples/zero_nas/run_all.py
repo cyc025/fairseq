ac4e8a34ac406534eea0d5ade68581fc585cfe25
deeploid <erniecyc@gmail.com>
2021-07-04 10:30:46 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -75,10 +75,10 @@ for tups in params_list[:1]:
 print(result_list)
 
 import pickle
-with open(f'{argv[1]}_results.pkl', 'wb') as handle:
+with open(f'{sys.argv[1]}_results.pkl', 'wb') as handle:
     pickle.dump(sorted_max_zen_tup, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-with open(f'{argv[1]}_results.pkl', 'rb') as handle:
+with open(f'{sys.argv[1]}_results.pkl', 'rb') as handle:
     b = pickle.load(handle)
 
 print(b)
