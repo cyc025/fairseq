2feda08a8b58a27c5a97b99ad33cf7b1a3d18713
deeploid <erniecyc@gmail.com>
2021-07-04 10:26:09 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -11,7 +11,6 @@ import pickle
 def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
     # clear checkpoints
     os.system("rm checkpoints/transformer_wikitext-2/*")
-
     params = {
         'decoder_embed_dim': decoder_embed_dim,
         'decoder_layers': decoder_layers,
@@ -21,7 +20,7 @@ def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
                    --decoder-layers {params['decoder_layers']} \
                    --decoder-attention-heads {params['decoder_attention_heads']} "
 
-    search_command = f"python train.py --task language_modeling \
+    train_command = f"python train.py --task language_modeling \
               data-bin/wikitext-2 \
               --decoder-normalize-before \
               --save-dir checkpoints/transformer_wikitext-2 \
@@ -35,12 +34,19 @@ def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
               --fp16 \
               --batch-size 1024 \
               {parameters} \
-              --max-update 10"
-    print(search_command)
-    os.system(search_command)
+              --max-update 100 --no-epoch-checkpoints"
+    os.system(train_command)
 
-    perplexity_score = float(open('.perplexity.log','r').read().strip())
+    eval_command = f"fairseq-eval-lm data-bin/wikitext-2 \
+                    --path checkpoints/transformer_wikitext-2/checkpoint_last.pt \
+                    --batch-size 2 \
+                    --tokens-per-sample 512 \
+                    --context-window 400 | grep \"Perplexity: \" > .pp.log"
+    os.system(eval_command)
+    s = open('.pp.log','r').read()
+    perplexity_score = get_perplexity(s)
     return perplexity_score
+
 
 def postprocess(field):
     params = []
@@ -48,12 +54,19 @@ def postprocess(field):
         params.append(i.split(': ')[-1])
     return params
 
+
+def get_perplexity(s):
+    s = "2021-07-04 10:06:33 | INFO | fairseq_cli.eval_lm | Loss (base 2): 10.0412, Perplexity: 1053.69"
+    pp = s.split('Perplexity: ')[-1]
+    return pp
+
+
 with open(f'{sys.argv[1]}.pkl', 'rb') as handle:
     params_list = pickle.load(handle)
 
 # run all
 result_list = []
-for tups in params_list:
+for tups in params_list[:1]:
     decoder_embed_dim,decoder_layers,decoder_attention_heads = postprocess(tups[0][2])
     perplexity_score = run(decoder_embed_dim,decoder_layers,decoder_attention_heads)
     tups.append(perplexity_score)
