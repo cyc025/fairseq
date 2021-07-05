1b8a001cd751db5b019cf887d85dfc1b8255336d
deeploid <erniecyc@gmail.com>
2021-07-05 09:12:20 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -27,7 +27,7 @@ def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
               --arch transformer_lm --share-decoder-input-output-embed \
               --dropout 0.1 \
               --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0 \
-              --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
+              --lr 0.00005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
               --tokens-per-sample 512 --sample-break-mode none \
               --max-tokens 2048 --update-freq 16 \
               --fp16 \
