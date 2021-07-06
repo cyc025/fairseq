4cf43ecd811b168f74112816aae345a0e46531ab
deeploid <erniecyc@gmail.com>
2021-07-06 10:19:57 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -8,6 +8,7 @@ import os
 import math
 import pickle
 
+
 def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
     # clear checkpoints
     os.system("rm checkpoints/transformer_wikitext-2/*")
@@ -64,7 +65,7 @@ with open(f'{sys.argv[1]}.pkl', 'rb') as handle:
 
 # run all
 result_list = []
-for i in range(0,len(params_list),5):
+for i in range(1,len(params_list),5):
     tups = params_list[i]
     decoder_embed_dim,decoder_layers,decoder_attention_heads = postprocess(tups[0][2])
     perplexity_score = run(decoder_embed_dim,decoder_layers,decoder_attention_heads)
