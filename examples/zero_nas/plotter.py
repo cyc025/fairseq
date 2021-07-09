ad6baf5bdc079c1855f373c14bed914a6f726efa
deeploid <erniecyc@gmail.com>
2021-07-09 19:00:26 +0200

    fix errors

--- examples/zero_nas/plotter.py
+++ examples/zero_nas/plotter.py
@@ -10,15 +10,17 @@ plt.style.use('ggplot')
 
 x = []
 y = []
+z = []
 
-with open('results/model_zen.csv','r') as csvfile:
+with open('results/zen_model_perp.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in list(plots)[1:]:
         x.append(float(row[0]))
         y.append(float(row[1]))
+        z.append(float(row[2]))
 
 correlation, p_value = scipy.stats.pearsonr(x, y)
-plt.plot(x,y,label=f'Correlation: {round(correlation,4)},\nP value: {round(p_value,100)}', marker="x")
+plt.plot(x,y,label=f'Correlation: {round(correlation,4)}\nP value: {round(p_value,100)}', marker="x")
 
 
 # plt.xlabel('Zen-score')
@@ -27,6 +29,14 @@ plt.plot(x,y,label=f'Correlation: {round(correlation,4)},\nP value: {round(p_val
 plt.xlabel('Model Size')
 plt.ylabel('Zen-score')
 plt.title('Model size vs. Zen-score')
+
+# 
+#
+# fig, ax = plt.subplots()
+# ax.scatter(x, y)
+
+for i, txt in enumerate(z):
+    plt.annotate(txt, (x[i], y[i]))
 
 
 plt.legend()
