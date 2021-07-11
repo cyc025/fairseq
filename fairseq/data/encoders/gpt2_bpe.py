1bfcc648d38254a6987caaf85471c79596f0b303
deeploid <erniecyc@gmail.com>
2021-07-11 08:20:24 +0200

    fix errors

--- fairseq/data/encoders/gpt2_bpe.py
+++ fairseq/data/encoders/gpt2_bpe.py
@@ -36,6 +36,7 @@ class GPT2BPE(object):
     def encode(self, x: str) -> str:
         # from fairseq import pdb; pdb.set_trace()
         a = list(map(str, self.bpe.encode(x)))[:600]
+        print(len(a))
         return " ".join(a)
 
     def decode(self, x: str) -> str:
