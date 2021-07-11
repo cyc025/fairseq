1277301228b5c914b215fdeb82ae9dc7040eb116
deeploid <erniecyc@gmail.com>
2021-07-11 10:06:15 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -311,7 +311,7 @@ class SequenceGenerator(nn.Module):
         else:
             original_batch_idxs = torch.arange(0, bsz).type_as(tokens)
 
-        for step in range(0,max_len + 1,2):  # one extra step for EOS marker
+        for step in range(max_len + 1):  # one extra step for EOS marker
             # reorder decoder internal states based on the prev choice of beams
             if reorder_state is not None:
                 if batch_idxs is not None:
