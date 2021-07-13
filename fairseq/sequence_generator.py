00cd5808d151b83fff6986c5b9173777c8f18346
deeploid <erniecyc@gmail.com>
2021-07-13 19:47:21 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -437,7 +437,7 @@ class SequenceGenerator(nn.Module):
 
             # finalize hypotheses that end in eos
             # Shape of eos_mask: (batch size, beam size)
-            if step<self.max_len:
+            if step>3:
                 eos_mask = cand_scores.ne(cand_scores)
             else:
                 eos_mask = cand_scores.ne(-math.inf)
