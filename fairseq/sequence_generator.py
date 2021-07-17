adfc569fc0b1c927e67e23026524ae9f3ad352d7
deeploid <erniecyc@gmail.com>
2021-07-17 09:24:53 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -438,7 +438,7 @@ class SequenceGenerator(nn.Module):
             # and dimensions: [bsz, cand_size]
             cand_bbsz_idx = cand_beams.add(bbsz_offsets)
 
-            # from fairseq import pdb; pdb.set_trace()
+            cand_bbsz_idx[0] = cand_bbsz_idx[0][cand_bbsz_idx!=self.pad]
 
             # finalize hypotheses that end in eos
             # Shape of eos_mask: (batch size, beam size)
