ab4073d0a12550c93b22455e6363b5b25d3a2750
deeploid <erniecyc@gmail.com>
2021-07-18 17:35:45 +0200

    fix errors

--- fairseq/modules/transformer_layer.py
+++ fairseq/modules/transformer_layer.py
@@ -410,6 +410,7 @@ class TransformerDecoderLayer(nn.Module):
             else:
                 self_attn_state = [saved_state["prev_key"], saved_state["prev_value"]]
             return x, attn, self_attn_state
+        print(x.size())
         return x, attn, None
 
     def make_generation_fast_(self, need_attn: bool = False, **kwargs):
