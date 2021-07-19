00b1d0a97c02f2a398fc850f351caa1bac8cf7df
deeploid <erniecyc@gmail.com>
2021-07-19 09:07:37 +0200

    fix errors

--- eval.sh
+++ eval.sh
@@ -4,5 +4,6 @@ export DATAPATH=~/fairseq_cnn_data/cnn_cln
 # Tokenize hypothesis and target files.
 cat $DATAPATH/test.hypo | java -cp "*" edu.stanford.nlp.process.PTBTokenizer -ioFileList -preserveLines > $DATAPATH/test.hypo.tokenized
 cat $DATAPATH/test.target | java -cp "*" edu.stanford.nlp.process.PTBTokenizer -ioFileList -preserveLines > $DATAPATH/test.hypo.target
-files2rouge $DATAPATH/test.hypo.tokenized $DATAPATH/test.hypo.target
+# files2rouge $DATAPATH/test.hypo.tokenized $DATAPATH/test.hypo.target
 # Expected output: (ROUGE-2 Average_F: 0.21238)
+rouge -f $DATAPATH/test.hypo.tokenized $DATAPATH/test.hypo.target
