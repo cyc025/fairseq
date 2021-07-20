334df3849bb3a6b7f83e4d267006ceb0dcb90a15
deeploid <erniecyc@gmail.com>
2021-07-20 18:39:53 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -519,10 +519,10 @@ class SequenceGenerator(nn.Module):
             eos_mask[:, :beam_size] = ~((~cands_to_ignore) & (~eos_mask[:, :beam_size]))
 
             cand_offsets_var = (
-                cand_offsets[: eos_mask.size(1)].unsqueeze(0).repeat(1,repeat_size) \
-                if step_size > 1 \
+                cand_offsets[: eos_mask.size(1)].unsqueeze(0).repeat(1,repeat_size)
+                if step_size > 1
                 else cand_offsets[: eos_mask.size(1)]
-            }
+            )
 
             active_mask = torch.add(
                 eos_mask.type_as(cand_offsets) * cand_size,
