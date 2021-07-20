6ff44feed877e44abfd19d8e36ff1dd4bc7703d8
deeploid <erniecyc@gmail.com>
2021-07-20 15:48:58 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -514,11 +514,11 @@ class SequenceGenerator(nn.Module):
             # eos_mask: [1, 16]
 
             from fairseq import pdb; pdb.set_trace()
-
+            repeat_size = step_size if step > 1 else 1
             eos_mask[:, :beam_size] = ~((~cands_to_ignore) & (~eos_mask[:, :beam_size]))
             active_mask = torch.add(
                 eos_mask.type_as(cand_offsets) * cand_size,
-                cand_offsets[: eos_mask.size(1)].unsqueeze(0).repeat(1,step_size), #cand_offsets[: eos_mask.size(1)],
+                cand_offsets[: eos_mask.size(1)].unsqueeze(0).repeat(1,repeat_size), #cand_offsets[: eos_mask.size(1)],
             )
 
             # get the top beam_size active hypotheses, which are just
