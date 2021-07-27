5d09cc6ea4f4dcdd707dd1a029d475d03ce3f392
Ernie Chang <erniecyc@fb.com>
2021-07-27 15:10:44 +0200

    fix errors

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ src=de
 tgt=en
 
 python3 fairseq_cli/generate.py ${data_dir} --path ${save_path}/checkpoint_best.pt \
-    --task translation --remove-bpe --max-sentences 20 --source-lang ${src} --target-lang ${tgt} \
+    --remove-bpe --max-sentences 20 --source-lang ${src} --target-lang ${tgt} \
     --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --iter-decode-with-beam 1 --gen-subset test \
     --skip-invalid-size-inputs-valid-test --profile --num-workers	32 --distributed-world-size	4 \
     --results-path data_bin	\
