470ea55d18f99ca508ce7d539259399f133a06d2
Ernie Chang <erniecyc@fb.com>
2021-07-27 15:13:18 +0200

    fix errors

--- generate.sh
+++ generate.sh
@@ -6,12 +6,18 @@ data_dir=~/fairseq/data-bin/iwslt14.tokenized.de-en
 save_path=saved_models
 src=de
 tgt=en
+# 
+# python3 fairseq_cli/generate.py ${data_dir} --path ${save_path}/checkpoint_best.pt \
+#     --remove-bpe --max-sentences 20 --source-lang ${src} --target-lang ${tgt} \
+#     --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --iter-decode-with-beam 1 --gen-subset test \
+#     --skip-invalid-size-inputs-valid-test --profile --num-workers	32 --distributed-world-size	4 \
+#     --results-path data_bin	\
 
-python3 fairseq_cli/generate.py ${data_dir} --path ${save_path}/checkpoint_best.pt \
-    --remove-bpe --max-sentences 20 --source-lang ${src} --target-lang ${tgt} \
-    --iter-decode-max-iter 0 --iter-decode-eos-penalty 0 --iter-decode-with-beam 1 --gen-subset test \
-    --skip-invalid-size-inputs-valid-test --profile --num-workers	32 --distributed-world-size	4 \
-    --results-path data_bin	\
+
+fairseq-generate \
+    $data_dir --num-workers	32 \
+    --path ${save_path}/checkpoint_best.pt \
+    --beam 5 --remove-bpe
 
 
 # --quiet
