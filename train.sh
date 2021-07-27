cf41b626309aa9f7ca37b2c13857ba89f6ab758e
Ernie Chang <erniecyc@fb.com>
2021-07-27 14:46:20 +0200

    fix errors

--- train.sh
+++ train.sh
@@ -27,14 +27,14 @@ save_path=saved_models
 #     --max-update 10000  \
 
 python3 train.py ${data_dir} \
-    --save-dir ${save_path} \
+    --save-dir ${save_path} --fp16 \
     --arch transformer_wmt_en_de --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
-    --encoder-layers 12 --encoder-embed-dim 512 --decoder-layers 1 --decoder-embed-dim 512  \
+    --encoder-layers 6 --encoder-embed-dim 512 --decoder-layers 6 --decoder-embed-dim 512  \
     --eval-bleu --no-epoch-checkpoints \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-detok moses \
