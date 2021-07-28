582b3cecb87780393bc785dcf1573e898eac81e0
Ernie Chang <erniecyc@fb.com>
2021-07-28 16:06:23 +0200

    fix errors

--- train.sh
+++ train.sh
@@ -27,8 +27,9 @@ save_path=saved_models
 #     --max-update 10000  \
 
 
-# 12-1: 27.24
-# 11-2: ?
+# 12-1-512: 27.24
+# 11-2-512: 29.38
+# 12-1-64:
 
 
 python3 train.py ${data_dir} \
@@ -39,7 +40,7 @@ python3 train.py ${data_dir} \
     --dropout 0.3 --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
-    --encoder-layers 11 --encoder-embed-dim 512 --decoder-layers 2 --decoder-embed-dim 512  \
+    --encoder-layers 12 --encoder-embed-dim 512 --decoder-layers 1 --decoder-embed-dim 64  \
     --eval-bleu --no-epoch-checkpoints \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-detok moses \
