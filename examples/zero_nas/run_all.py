a9814c490916dc1838c9e89a249a69103213d7bc
Ernie Chang <erniecyc@fb.com>
2021-08-07 08:07:44 +0200

    fix errors

--- examples/zero_nas/run_all.py
+++ examples/zero_nas/run_all.py
@@ -9,6 +9,10 @@ import math
 import pickle
 
 
+# python train.py --task language_modeling data-bin/wikitext-2 --decoder-normalize-before --save-dir checkpoints/transformer_wikitext-2 --arch transformer_lm --share-decoder-input-output-embed --dropout 0.1 --optimizer adam --adam-betas '(0.9, 0.98)' --weight-decay 0.01 --clip-norm 0.0 --lr 0.00005 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 --tokens-per-sample 512 --sample-break-mode none --max-tokens 2048 --update-freq 16 --fp16 --batch-size 1024 \
+# --decoder-embed-dim 50 --decoder-layers 2 --decoder-attention-heads 2  --max-update 8000 --no-epoch-checkpoints --keep-best-checkpoints 1 --tensorboard-logdir tensorboard
+
+
 def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
     # clear checkpoints
     os.system("rm checkpoints/transformer_wikitext-2/*")
@@ -35,20 +39,17 @@ def run(decoder_embed_dim,decoder_layers,decoder_attention_heads):
               --batch-size 1024 \
               {parameters} \
               --max-update 8000 --no-epoch-checkpoints --keep-best-checkpoints 1"
+    os.system(train_command)
 
-    print(train_command)
-    #
-    # os.system(train_command)
-    #
-    # eval_command = f"fairseq-eval-lm data-bin/wikitext-2 \
-    #                 --path checkpoints/transformer_wikitext-2/checkpoint_best.pt \
-    #                 --batch-size 2 \
-    #                 --tokens-per-sample 512 \
-    #                 --context-window 400 | grep \"Perplexity: \" > .pp.log"
-    # os.system(eval_command)
-    # s = open('.pp.log','r').read()
-    # perplexity_score = get_perplexity(s)
-    # return perplexity_score
+    eval_command = f"fairseq-eval-lm data-bin/wikitext-2 \
+                    --path checkpoints/transformer_wikitext-2/checkpoint_best.pt \
+                    --batch-size 2 \
+                    --tokens-per-sample 512 \
+                    --context-window 400 | grep \"Perplexity: \" > .pp.log"
+    os.system(eval_command)
+    s = open('.pp.log','r').read()
+    perplexity_score = get_perplexity(s)
+    return perplexity_score
 
 
 def postprocess(field):
