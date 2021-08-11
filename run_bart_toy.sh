0ebb031e4bc4855c5d6c79cc654099c4bff35106
Ernie Chang <erniecyc@fb.com>
2021-08-11 22:27:47 +0200

    fix errors

--- run_bart_toy.sh
+++ run_bart_toy.sh
@@ -4,7 +4,7 @@
 ROOT=~/fairseq
 
 
-python examples/bart/summarize.py   --model-dir ${ROOT}/.   \
+python examples/bart/summarize.py   --model-dir .   \
 --model-file ${ROOT}/checkpoints/bart.large.cnn/model.pt    \
 --src ~/fairseq_cnn_data/cnn_cln/toy_me.source    \
 --out ~/fairseq_cnn_data/cnn_cln/test.hypo;
