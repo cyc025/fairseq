e45b52dee7922ea997e8666066f5775c6a2d6fc3
Ernie Chang <erniecyc@fb.com>
2021-08-12 13:04:49 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -560,9 +560,6 @@ class SequenceGenerator(nn.Module):
                 step,
             )
 
-            from fairseq import pdb; pdb.set_trace()
-
-
             if self.lm_model is not None:
                 lm_out = self.lm_model(tokens[:, : step + 1])
                 probs = self.lm_model.get_normalized_probs(
