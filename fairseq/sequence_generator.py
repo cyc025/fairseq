4c55f992352fb2ebbc2fcc924b8cd2fa8fb68965
Ernie Chang <erniecyc@fb.com>
2021-08-12 18:54:34 +0200

    fix errors

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -459,7 +459,7 @@ class SequenceGenerator(nn.Module):
         input_step_size = 2
 
         step_size = input_step_size if input_step_size < max_len else max_len
-        step_max_len = int( max_len / step_size )
+        step_max_len = int( max_len / step_size ) + 1
         for step in range(0,step_max_len):  # one extra step for EOS marker
 
             # reorder decoder internal states based on the prev choice of beams
